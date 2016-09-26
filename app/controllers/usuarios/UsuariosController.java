e162040c5badb09d73069d7f5c341865ced091c9
Angel Daniel Barrera <angelp77@gmail.com>
2016-09-26 16:16:42 -0500

    Servicio rest Registrar Usuario

---
+++ app/controllers/usuarios/UsuariosController.java
@@ -0,0 +1,63 @@
+package controllers.usuarios;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.Counter;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class contains an
+ * action that shows an incrementing count to users. The {@link Counter}
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+public class UsuariosController extends Controller {
+
+    @Inject
+    public UsuariosController(Counter counter) {
+
+    }
+
+    /**
+     * An action that responds with the {@link Counter}'s current
+     * count. The result is plain text. This action is mapped to
+     * <code>GET</code> requests with a path of <code>/count</code>
+     * requests by an entry in the <code>routes</code> config file.
+     */
+    public Result registroGet() {
+        return ok("ok prueba Get");
+    }
+
+    public Result registroPost() {
+        JsonNode json = request().body().asJson();
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String name = json.findPath("name").textValue();
+            if(name == null) {
+                return badRequest("Missing parameter [name]");
+            } else {
+                return ok("Hello " + name);
+            }
+        }
+    }
+    public Result registro() {
+        JsonNode json = request().body().asJson();
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String name = json.findPath("name").textValue();
+            String correo = json.findPath("correo").textValue();
+            String contraseña = json.findPath("contraseña").textValue();
+            if(name == null | correo == null | contraseña == null) {
+                return badRequest("Missing parameter");
+            } else {
+                return ok("OK");
+            }
+        }
+    }
+}
