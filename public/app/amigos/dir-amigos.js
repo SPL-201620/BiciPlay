f0184de0f888451a4f500716d0955848784ccde9
Diego Andrés <andruco3@gmail.com>
2016-10-02 13:30:45 -0500

    Agregar, buscar amigos

--- public/app/amigos/dir-amigos.js
+++ public/app/amigos/dir-amigos.js
@@ -1,19 +1,38 @@
-(function(){
-  angular.module('app').directive('dirAmigos', function() {
-  	return {
-  		restrict: 'E',
-  		scope: {
-  		},
-  		templateUrl: 'app/amigos/dir-amigos.html',
-  		controller: controller
-  	};
-  });
+(function() {
+    angular.module('app').directive('dirAmigos', function() {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'app/amigos/dir-amigos.html',
+            controller: controller
+        };
+    });
 
 
-  function controller($scope, Amigos) {
-  	//Amigos.getAmigos().then(function(amigosP){
-      //$scope.amigos = amigosP;
-    //});
+    function controller($scope, Amigos) {
+        $scope.$watch('busqueda', function(busqueda) {
+            if (!busqueda || busqueda === "") {
+              $scope.personas = null;
+            }
+        });
+        refreshAmigos();
 
-  }
+        $scope.buscarPersonas = function(nombre) {
+            Amigos.buscarPersonas(nombre).then(function(personas) {
+                $scope.personas = personas;
+            })
+        };
+        $scope.agregarAmigo = function(persona){
+          Amigos.agregarAmigo(persona.id).then(function(personas) {
+              refreshAmigos();
+          })
+        };
+        function refreshAmigos(){
+          Amigos.getAmigos().then(function(amigosP) {
+              $scope.amigos = amigosP;
+          });
+        }
+
+
+    }
 })();
