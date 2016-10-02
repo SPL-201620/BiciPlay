d938e4711109739c37ac6389c54c7e728ff2ed61
Diego Andrés <andruco3@gmail.com>
2016-10-02 09:38:44 -0500

    Cambios SQL  Tabla amigos y chat

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,10 +3,20 @@
 
 # --- !Ups
 
+create table chat (
+  id                            integer not null,
+  id_usuario1                   varchar(255),
+  id_usuario2                   varchar(255),
+  fecha_hora                    timestamp,
+  mensaje                       varchar(255),
+  constraint pk_chat primary key (id)
+);
+create sequence chat_seq;
+
 create table ubicacion (
   id                            integer not null,
-  lat                           float,
-  lng                           float,
+  lat                           double,
+  lng                           double,
   nombre                        varchar(255),
   constraint pk_ubicacion primary key (id)
 );
@@ -22,12 +32,35 @@ create table usuario (
 );
 create sequence usuario_seq;
 
+create table amigos (
+  usuarioid                     integer not null,
+  amigoid                       integer not null,
+  constraint pk_amigos primary key (usuarioid,amigoid)
+);
+
+alter table amigos add constraint fk_amigos_usuario_1 foreign key (usuarioid) references usuario (id) on delete restrict on update restrict;
+create index ix_amigos_usuario_1 on amigos (usuarioid);
+
+alter table amigos add constraint fk_amigos_usuario_2 foreign key (amigoid) references usuario (id) on delete restrict on update restrict;
+create index ix_amigos_usuario_2 on amigos (amigoid);
+
 
 # --- !Downs
 
-drop table if exists ubicacion cascade;
+alter table amigos drop constraint if exists fk_amigos_usuario_1;
+drop index if exists ix_amigos_usuario_1;
+
+alter table amigos drop constraint if exists fk_amigos_usuario_2;
+drop index if exists ix_amigos_usuario_2;
+
+drop table if exists chat;
+drop sequence if exists chat_seq;
+
+drop table if exists ubicacion;
 drop sequence if exists ubicacion_seq;
 
-drop table if exists usuario cascade;
+drop table if exists usuario;
 drop sequence if exists usuario_seq;
+
+drop table if exists amigos;
 
